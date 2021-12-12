8fdc307d4f6c5b030ac3f1e368c13e760cc6d66b
Jim Arment <jima@zillowgroup.com>
2021-12-12 05:57:40 -0800

    Remove unused code

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -9,28 +9,6 @@ import { imageUrlFor } from "../lib/image-url";
 import { buildImageObj } from "../lib/helpers";
 
 export const query = graphql`
-  fragment SanityImage on SanityMainImage {
-    crop {
-      _key
-      _type
-      top
-      bottom
-      left
-      right
-    }
-    hotspot {
-      _key
-      _type
-      x
-      y
-      height
-      width
-    }
-    asset {
-      _id
-    }
-  }
-
   query IndexPageQuery {
     site: sanitySiteSettings(_id: { regex: "/(drafts.|)siteSettings/" }) {
       title
